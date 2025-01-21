ea8ebdfbc235d3a00baab740305cbe89baad28c4
Heinrich Ahrend <h.ahrend@unibas.ch>
2025-01-21 13:06:10 +0100

    Let motor stand still if both sensors detect signal

--- code/arduino.ino
+++ code/arduino.ino
@@ -1,20 +1,18 @@
 #define stepPin 2
 #define dirPin 5
+#define motionPin1 6
+#define motionPin2 4
 
-
-int MOTION = 6;
-int STATE = 0;
-
-int MOTION2 = 4;
-int STATE2 = 0;
+int state1 = 0;
+int state2 = 0;
 
 int customDelay, customDelayMapped;
 
 void setup()
 {
   
-  pinMode(MOTION, INPUT);
-  pinMode(MOTION2, INPUT);
+  pinMode(motionPin1, INPUT);
+  pinMode(motionPin2, INPUT);
   Serial.begin(9600);
 
   pinMode(stepPin, OUTPUT);
@@ -23,64 +21,50 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
-
-  STATE = digitalRead(MOTION);
-  STATE2 = digitalRead(MOTION2);
-
-  if (STATE == HIGH) //If Sensor1 = HIGH
-  {
-    Serial.println("sensor one");
-    digitalWrite(dirPin, HIGH);
-    for (int i = 0; i < 60; i++) { //turn quater
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(2000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(2000);
+void loop() {
+  state1 = 0;
+  state2 = 0;
+  Serial.println("Detection started");
+  for (int i = 0; i < 200; i++) {
+    if (digitalRead(motionPin1) == HIGH) {
+      state1 = 1;
     }
-    delay(1000);
-    
-    takepicture();
-
-    digitalWrite(dirPin, LOW); //change direction i guess?
-    for (int i = 0; i < 60; i++) { //turn quater back
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(2000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(2000);
+    if (digitalRead(motionPin2) == HIGH) {
+      state2 = 1;
     }
-    delay(1000);
-    Serial.println("finished sensor one");
-
-  } if (STATE2 == HIGH) { //I Sensor2 = HIGH
-    Serial.println("sensor two detected");
-    digitalWrite(dirPin, LOW); //change direction i guess?
-    for (int i = 0; i < 100; i++) { //turn quater
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(2000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(2000);
-    }
-    delay(1000);
-
-    takepicture();
-
-    digitalWrite(dirPin, HIGH);
-    for (int i = 0; i < 60; i++) { //turn quater back
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(2000);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(2000);
-    }
-    delay(1000);
-    Serial.println("finished sensor two");
+    delay(10);
   }
+  Serial.println("Detection ended");
 
+  if (state1 == HIGH && state2 == HIGH) {// Movement in the middle
+    Serial.println("Both sensors");
+    takePicture();
+  } else if (state1 == HIGH) {
+    Serial.println("Only Sensor 1");
+    moveInDirection(1);
+    takePicture();
+    moveInDirection(0);
+  } else if (state2 == HIGH) {
+    Serial.println("Only Sensor 2");
+    moveInDirection(0);
+    takePicture();
+    moveInDirection(1);
+  }
+  delay(500);
 }
 
-void takepicture(){
+void moveInDirection(int dir) {
+  digitalWrite(dirPin, dir);
+  for (int i = 0; i < 60; i++) { //turn quater
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(5000);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(5000);
+  }
+  delay(1000);
+}
 
+void takePicture() {
    //take picture
     digitalWrite(7, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
